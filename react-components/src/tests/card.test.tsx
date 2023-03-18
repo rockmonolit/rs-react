b28309a2bf7ad0d8adf703fbe07954bc4a04cc76
rockmonolit <rockmonolit@mail.ru>
2023-03-18 19:16:53 +0300

    feat: improve card styling

--- react-components/src/tests/card.test.tsx
+++ react-components/src/tests/card.test.tsx
@@ -6,6 +6,6 @@ import Card from '../components/card';
 
 test('card component should contain provided info', () => {
   render(<Card author="test author" title="test title" description="test description" />);
-  const testText = screen.getByText(/test author/i);;
+  const testText = screen.getByText(/test author/i);
   expect(testText).toBeInTheDocument();
 });
