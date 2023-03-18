47426cfc5014221f6a928864e5d8c05b4887f545
rockmonolit <rockmonolit@mail.ru>
2023-03-18 20:28:45 +0300

    refactor: extract test mock data to separate variable

--- react-components/src/tests/card.test.tsx
+++ react-components/src/tests/card.test.tsx
@@ -4,8 +4,10 @@ import '@testing-library/jest-dom';
 
 import Card from '../components/card';
 
+const mockedCard = { author: 'test author', title: 'test title', description: 'test description' };
+
 test('card component should contain provided info', () => {
-  render(<Card author="test author" title="test title" description="test description" />);
+  render(<Card author={mockedCard.author} title={mockedCard.title} description={mockedCard.description} />);
   const testText = screen.getByText(/test author/i);
   expect(testText).toBeInTheDocument();
 });
