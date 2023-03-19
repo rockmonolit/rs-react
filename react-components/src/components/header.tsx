3004ffd06afec90bcdc6f8a8220b0b8a68ab17d6
rockmonolit <rockmonolit@mail.ru>
2023-03-19 11:02:27 +0300

    refactor: separate index.scss file into modules

--- react-components/src/components/header.tsx
+++ react-components/src/components/header.tsx
@@ -13,19 +13,18 @@ class Header extends React.Component<HeaderProps> {
   render() {
     return (
       <header className="header">
-        <div className="headerContent">
-          <nav className="navbar">
-            <NavLink to="/main" className="linkBtn btnMain">
-              Main
-            </NavLink>
-            <NavLink to="/about" className="linkBtn btnAbout">
-              About Us
-            </NavLink>
-            <NavLink to="/404" className="linkBtn btnError">
-              404
-            </NavLink>
-          </nav>
-          <p className="currentRoute headerText">Current page: {this.props.currentPage}</p>
+        <div className="wrapper">
+          <div className="headerContent">
+            <nav className="navbar">
+              <NavLink to="/main" className="linkBtn btnMain">
+                Main
+              </NavLink>
+              <NavLink to="/about" className="linkBtn btnAbout">
+                About Us
+              </NavLink>
+            </nav>
+            <p className="currentRoute headerText">Current page: {this.props.currentPage}</p>
+          </div>
         </div>
       </header>
     );
