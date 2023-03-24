41cacda694ee6faa0630e87aeec582b84b1fc61d
rockmonolit <rockmonolit@mail.ru>
2023-03-24 17:18:19 +0300

    feat: add modal window

--- react-forms/src/components/formPage/FormPage.tsx
+++ react-forms/src/components/formPage/FormPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../header';
 import Form from '../form/Form';
+import FormModal from './FormModal';
 
 function FormPage() {
   return (
@@ -8,6 +9,7 @@ function FormPage() {
       <Header currentPage="Form" />
       <div className="wrapper">
         <h1>Please, fill the form below</h1>
+        <FormModal />
         <Form />
         <div className="cardsContainer formCardsContainer"></div>
       </div>
