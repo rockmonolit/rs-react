b7a2ae6500fbcee4bbbf50455474c5e7f3c03b63
rockmonolit <rockmonolit@mail.ru>
2023-04-24 14:42:50 +0300

    fix: change vite config

--- react-components/vite.config.ts
+++ react-components/vite.config.ts
@@ -1,20 +1,16 @@
-import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import eslint from 'vite-plugin-eslint';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(),
-    eslint(),],
-  root: "src",
   build: {
     sourcemap: true,
-    minify: false,
   },
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      '@': resolve(__dirname, 'src'),
     },
   },
 });
