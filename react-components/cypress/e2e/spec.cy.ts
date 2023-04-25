5753fcea493296d1288164c1b1edf881d9857adf
rockmonolit <rockmonolit@mail.ru>
2023-04-25 21:43:22 +0300

    feat: add first cypress test

---
+++ react-components/cypress/e2e/spec.cy.ts
@@ -0,0 +1,5 @@
+describe('First Test', () => {
+  it('Visits the main page', () => {
+    cy.visit('/');
+  });
+});
--- react-components/vite.config.ts
+++ react-components/vite.config.ts
@@ -4,13 +4,21 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  build: {
-    sourcemap: true,
-  },
+  // build: {
+  //   sourcemap: true,
+  // },
   plugins: [react(), tsconfigPaths()],
+  // css: {
+  //   devSourcemap: true,
+  // },
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    watch: {
+      ignored: ['/coverage/'],
     },
   },
 });
